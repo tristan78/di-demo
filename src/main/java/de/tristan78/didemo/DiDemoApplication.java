f63e2126d03755a74baaf9dabc0de94e06dda5cd
Tristan78 <tristan78@gmx.de>
2018-10-24 22:48:29 +0200

    After 35. The Spring Framework Context

--- src/main/java/de/tristan78/didemo/DiDemoApplication.java
+++ src/main/java/de/tristan78/didemo/DiDemoApplication.java
@@ -2,11 +2,18 @@ package de.tristan78.didemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import de.tristan78.didemo.controllers.MyController;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(DiDemoApplication.class, args);
+		
+		MyController controller = (MyController) context.getBean("myController");
+		
+		controller.hello();
 	}
 }
---
+++ src/main/java/de/tristan78/didemo/controllers/MyController.java
@@ -0,0 +1,14 @@
+package de.tristan78.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+	
+	public String hello() {
+		System.out.println("Hello!!!");
+		
+		return "foo";
+	}
+
+}
