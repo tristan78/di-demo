1962187160e8adf9360bdd844ddf031f1a2a05c8
Tristan78 <tristan78@gmx.de>
2018-11-04 14:51:52 +0100

    After 42. Default Profile Behavior for Spring Context

--- src/main/java/de/tristan78/didemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/de/tristan78/didemo/services/PrimaryGermanGreetingService.java
@@ -4,14 +4,13 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-
 @Service
 @Primary
-@Profile({"en", "default"})
-public class PrimaryGreetingService implements GreetingService {
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
 
-    @Override
-    public String sayGreeting() {
-        return "Hello - Primary Greeting service";
-    }
+	@Override
+	public String sayGreeting() {
+		return "Primärer Grußdienst";
+	}
 }
